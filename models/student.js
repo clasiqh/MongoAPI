bd9a356efbb113e31ba271fd12e6149be64fa26c
clasiqh <41128055+clasiqh@users.noreply.github.com>
2022-01-26 00:34:59 +0530

    removed unnecessary comment

--- models/student.js
+++ models/student.js
@@ -1,41 +1,5 @@
 const mongoose = require("mongoose");
 
-/*const mySchema = new mongoose.Schema({
-  startDate: String,
-  businessID: {
-    type: Object,
-    required: false,
-  },
-  checkList: Object,
-  city: Object,
-  createdAt: String,
-  createdBy: String,
-  droppedDate: String,
-  gigOrderStatus: String,
-  gigerClientId: {
-    type: String,
-    required: false,
-  },
-  gigerId: String,
-  gigerName: String,
-  isActive: String,
-  jobProfile: Object,
-  lastWorkingDate: {
-    type: String,
-    required: false,
-  },
-  reportingLocation: Object,
-  secondaryMobileNumber: {
-    type: Number,
-    required: false,
-  },
-  status: {
-    type: String,
-    required: false,
-  },
-});
-*/
-
 const mySchema = new mongoose.Schema({
   name: String,
   roll_no: Number,
